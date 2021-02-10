283769c92093e4c269fd6eabc270d7d7de195d65
Claudio <claudioeduardobardales@gmail.com>
2021-02-09 21:39:09 -0800

    Desktop Skills section done

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -12,12 +12,15 @@ import Styled from "styled-components"
 const Skills = () => {
   return (
     <StyledSection>
-      <FaHtml5 />
-      <FaCss3Alt />
-      <FaJsSquare />
-      <FaGitAlt />
-      <FaReact />
-      <SiStyledComponents />
+      <h1>MY SKILLS!</h1>
+      <Container>
+        <FaHtml5 />
+        <FaCss3Alt />
+        <FaJsSquare />
+        <FaGitAlt />
+        <FaReact />
+        <SiStyledComponents />
+      </Container>
     </StyledSection>
   )
 }
@@ -26,8 +29,21 @@ const StyledSection = Styled.div`
 min-height: 50vh;
 background-color: #000;
 color: #fff;
-display:grid;
-
+h1{
+  margin: auto;
+  width: 50%;
+  padding: 3rem;
+  text-align: center;
+}
+`
+const Container = Styled.div`
+display: grid;
+grid-template-columns: repeat(3,1fr);
+font-size: 6rem;
+grid-gap: 4rem;
+width: 50%;
+margin: 0 auto;
+justify-items: center;
 `
 
 export default Skills
