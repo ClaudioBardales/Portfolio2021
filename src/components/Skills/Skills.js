2c48da35988562afabb9675adb4c3266f14ad7aa
Claudio <claudio@Claudios-Mac.local>
2021-10-17 14:50:41 -0700

    added labels to icons

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -14,19 +14,37 @@ const Skills = () => {
     <StyledSection>
       <h1>My Skills!</h1>
       <Container>
-        <FaHtml5 />
-        <FaCss3Alt />
-        <FaJsSquare />
-        <FaGitAlt />
-        <FaReact />
-        <SiStyledComponents />
+        <div>
+          <FaHtml5 />
+          <h3>HTML5</h3>
+        </div>
+        <div>
+          <FaCss3Alt />
+          <h3>CSS3</h3>
+        </div>
+        <div>
+          <FaJsSquare />
+          <h3>JavaScript</h3>
+        </div>
+        <div>
+          <FaGitAlt />
+          <h3>GIT</h3>
+        </div>
+        <div>
+          <FaReact />
+          <h3>ReactJS</h3>
+        </div>
+        <div>
+          <SiStyledComponents />
+          <h3>Styled Components</h3>
+        </div>
       </Container>
     </StyledSection>
   )
 }
 
 const StyledSection = Styled.div`
-min-height: 50vh;
+min-height: 56vh;
 background-color: #000;
 color: #fff;
 h1{
@@ -48,6 +66,14 @@ justify-content: space-evenly;
 justify-items: center;
 align-content: space-evenly;
 align-items: center;
+div{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  h3{
+  font-size: 1.5rem;
+}
+}
 @media (max-width: 768px){
   grid-template-columns: repeat(2,1fr);
   font-size: 3.30rem;
