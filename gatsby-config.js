25c573aae2de4c2a556a1ecb97099dd190319971
Claudio <claudio@Claudios-Mac.local>
2022-01-17 19:30:26 -0800

    Fixed Raw html

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: ["gatsby-plugin-react-helmet"],
+  plugins: ["gatsby-plugin-react-helmet", "gatsby-plugin-styled-components"],
   siteMetadata: {
     title: "Claudio Bardales | Front End Developer",
     description: "A Front End Developer Portfolio of Claudio Bardales",
